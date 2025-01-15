9880b16ed2f77512328d9054bfd4f6bd343e3087
souvikchand <96312748+souvikchand@users.noreply.github.com>
2025-01-15 21:53:48 +0530

    Update parkinson.py

--- pages/parkinson.py
+++ pages/parkinson.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title('Parkinson disease :brain:')
 
 st.write('UI not prepared yet.:sweat_smile:')
+st.write('You can check the github')
+giturl= "https://github.com/souvikchand/multi-disease"
